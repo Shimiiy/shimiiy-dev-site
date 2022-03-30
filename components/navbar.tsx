ddc26b89f9f2803ec375e5e5ab55f2994c3a1921
Shimiiy <bangchaq2002@gmail.com>
2022-03-30 21:52:46 +0900

    added contact page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,23 @@
-const Navbar = () => {
+import React from "react";
+
+import Link from "next/link"
+
+const Navbar: React.FC = () => {
   return (
-    <nav className="text-3xl font-serif bg-sky-800 text-white p-2">
-      <ul className="flex space-x-5">
-        <li>Shimiiy / シミー</li>
-        <li>にゃーん</li>
+    <nav className="text-3xl font-serif bg-sky-800 text-white p-4">
+      <ul className="flex space-x-10">
+        <li>
+          <Link href="/">
+            <a>Shimiiy / シミー</a>
+          </Link>
+        </li>  
+        <li>About Me</li>
+        <li>Works</li>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
       </ul>
     </nav>
   )
---
+++ pages/contact.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Navbar from '../components/navbar'
+
+const Contact: NextPage = () => {
+  return (
+  <main>
+    <Navbar />
+    <p className='p-5'>
+      Hi!
+    </p>
+  </main>
+  )
+}
+
+
+export default Contact
