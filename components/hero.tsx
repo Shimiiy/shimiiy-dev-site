fd5140b8adf3f04c1351ec27d2eab7b3ba28d8e9
Shimiiy <bangchaq2002@gmail.com>
2022-10-02 00:47:29 +0900

    Added placeholder image to page

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,7 +7,7 @@ import Link from "next/link"
 const Hero = () => {
   return (
     // Flex Container
-    <div className="container flex flex-row items-center px-6 mx-auto pt-10 space-y-0 md:space-y-0">
+    <div className="container flex flex-col md:flex-row items-center px-6 mx-auto py-10 space-y-0 md:space-y-0">
       <div className="flex flex-col mb-32 mx-auto md:w-1/2 md:mx-0">
         <h1 className="max-w-md text-white text-4xl font-bold text-center mb-8 md:text-5xl md:text-left">Hi, I&apos;m <span className="text-yellowShim">Shimiiy</span></h1> {/* ' escaped: I&apos;m Shimiiy = I'm Shimiiy */}
         <p className="text-white text-center md:text-left">I&apos;m a computer science student at <span className="text-green-400">SIT</span> in Tokyo.</p>
@@ -19,7 +19,10 @@ const Hero = () => {
             </a>
           </Link>
         </div>
-        {/* <Image src={shimPlaceholderPic} alt="Shimiiy Placeholder"/> */}
+      </div>
+      <div>
+        <Image src={shimPlaceholderPic} alt="Shimiiy Placeholder" width={500} height={500}/>
+        <p className="text-white text-center">Editor&apos;s Note:（そのうち、上のこの画像の代わりに3Dモデルを置きたいね。）</p>
       </div>
     </div>
   )
