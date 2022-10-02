4982841e9ae05e2775a5ff22194221cf4dda77b4
Shimiiy <bangchaq2002@gmail.com>
2022-10-02 13:04:09 +0900

    Social media links are centered

--- components/hero.tsx
+++ components/hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
         <h1 className="max-w-md text-white text-4xl font-bold text-center mb-8 md:text-5xl md:text-left">Hi, I&apos;m <span className="text-yellowShim">Shimiiy</span></h1> {/* ' escaped: I&apos;m Shimiiy = I'm Shimiiy */}
         <p className="text-white text-center md:text-left">I&apos;m a computer science student at <span className="text-green-400">SIT</span> in Tokyo.</p>
         <p className="text-white text-center md:text-left">I also make <u>3D artworks</u> as a hobby.</p>
-        <div className="flex space-x-2 mt-3">
+        <div className="flex space-x-2 mt-3 mx-auto md:mx-0">
           <a href="https://www.youtube.com/channel/UClXWVfHTOeGPznaPYspWqeQ">
             <Image src={youtubeLogo} alt="YouTube" width={36} height={36} />
           </a>
