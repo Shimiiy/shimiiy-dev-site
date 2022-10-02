97a09644d22c0684653ad0f54d79a1820506d998
Shimiiy <bangchaq2002@gmail.com>
2022-10-02 12:56:43 +0900

    Set social media links

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,8 +1,13 @@
 import Box from "./box"
 import ContactButton from "./buttons/contactButton"
-import shimPlaceholderPic from "../public/shim_placeholder.png"
+
 import Image from "next/image"
 import Link from "next/link"
+
+import shimPlaceholderPic from "../public/shim_placeholder.png"
+import youtubeLogo from "../public/icons8-youtube.svg"
+import twitterLogo from "../public/icons8-twitter.svg"
+import githubLogo from "../public/icons8-github.svg"
 
 const Hero = () => {
   return (
@@ -12,7 +17,18 @@ const Hero = () => {
         <h1 className="max-w-md text-white text-4xl font-bold text-center mb-8 md:text-5xl md:text-left">Hi, I&apos;m <span className="text-yellowShim">Shimiiy</span></h1> {/* ' escaped: I&apos;m Shimiiy = I'm Shimiiy */}
         <p className="text-white text-center md:text-left">I&apos;m a computer science student at <span className="text-green-400">SIT</span> in Tokyo.</p>
         <p className="text-white text-center md:text-left">I also make <u>3D artworks</u> as a hobby.</p>
-        <div className="my-8 mx-auto md:w-1/2 md:mx-0">
+        <div className="flex space-x-4 mt-3">
+          <a href="https://www.youtube.com/channel/UClXWVfHTOeGPznaPYspWqeQ">
+            <Image src={youtubeLogo} alt="YouTube" width={36} height={36} />
+          </a>
+          <a href="https://twitter.com/shimiiy0113">
+            <Image src={twitterLogo} alt="Twitter" width={36} height={36} />
+          </a>
+          <a href="https://github.com/Shimiiy">
+            <Image src={githubLogo} alt="Github" width={36} height={36} />
+          </a>
+        </div>
+        <div className="my-6 mx-auto md:w-1/2 md:mx-0">
           <Link href="/contact">
             <a className="text-center p-5 py-3 text-white rounded-2xl bg-orangeShim hover:bg-orangeShimLighter">
               Contact Me!
@@ -21,11 +37,12 @@ const Hero = () => {
         </div>
       </div>
       <div>
-        <Image src={shimPlaceholderPic} alt="Shimiiy Placeholder" width={500} height={500}/>
-        <p className="text-white text-center">Editor&apos;s Note:（そのうち、上のこの画像の代わりに3Dモデルを置きたいね。）</p>
+        <Image src={shimPlaceholderPic} alt="Shimiiy Placeholder" width={400} height={400}/>
       </div>
     </div>
   )
 }
+
+// Icons by Icons8, remember to add this on footer
 
 export default Hero
