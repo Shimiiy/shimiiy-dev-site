83ee23b58d2868446626242bb3aba2b9fbeb7098
Shimiiy <bangchaq2002@gmail.com>
2022-10-02 16:03:20 +0900

    Margin between text & pic is smaller in mobile

--- components/hero.tsx
+++ components/hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
   return (
     // Flex Container
     <div className="container flex flex-col md:flex-row items-center px-6 mx-auto py-10 space-y-0 md:space-y-0">
-      <div className="flex flex-col mb-32 mx-auto md:w-1/2 md:mx-0">
+      <div className="flex flex-col mb-16 mx-auto md:w-1/2 md:mx-0 md:mb-32">
         <h1 className="max-w-md text-white text-4xl font-bold text-center mb-8 md:text-5xl md:text-left">Hi, I&apos;m <span className="text-yellowShim">Shimiiy</span></h1> {/* ' escaped: I&apos;m Shimiiy = I'm Shimiiy */}
         <p className="text-white text-center md:text-left">I&apos;m a computer science student at <span className="text-green-400">SIT</span> in Tokyo.</p>
         <p className="text-white text-center md:text-left">I also make <u>3D artworks</u> as a hobby.</p>
