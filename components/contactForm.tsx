5aea909c42b785df1163fcd61584778adb772b43
Shimiiy <bangchaq2002@gmail.com>
2022-10-09 18:30:57 +0900

    Send Email is now a button

--- components/contactForm.tsx
+++ components/contactForm.tsx
@@ -9,21 +9,23 @@ const ContactForm = () => {
       <div className="pt-20" /> {/* This div is here to prevent the text being covered by the curves */}
       <div className="container px-12 mx-auto py-10 space-y-0 md:space-y-0">
         <h1 className="font-bold text-4xl text-white md:mr-6">Send me an email</h1>
+        {/* Form */}
         <form action="" className="text-white">
           <div className="flex flex-col md:flex-row py-6 justify-between">
             <div className="w-full md:w-1/2 md:pr-6 pb-6">
               <label htmlFor="name">Name</label><br />
-              <input type="text" id="name" name="name" placeholder="John Smith" className="rounded-lg w-full mb-3 text-black text-left p-2"></input><br />
+              <input type="text" id="name" name="name" placeholder="John Smith" className="rounded-lg w-full mb-3 text-black text-left p-2" required></input><br />
               <label htmlFor="email">Email</label><br />
-              <input type="email" id="email" name="email" placeholder="johnsmith@email.com" className="rounded-lg w-full text-black p-2"></input><br />
+              <input type="email" id="email" name="email" placeholder="johnsmith@email.com" className="rounded-lg w-full text-black p-2" required></input><br />
             </div>
             <div className="w-full md:w-1/2 md:pl-6">
               <label htmlFor="message">Message</label><br />
-              <textarea id="message" name="message" rows={5} cols={33} className="rounded-lg w-full mb-6 text-black p-2"></textarea><br />
-              <input type="submit" value="Send Email" className="font-bold text-center p-6 py-3 text-white rounded-2xl bg-orangeShim hover:bg-orangeShimLighter"></input>      
+              <textarea id="message" name="message" rows={5} cols={33} className="rounded-lg w-full mb-6 text-black p-2" required></textarea><br />
+              <button type="submit" className="font-bold text-center p-6 py-3 text-white rounded-2xl bg-orangeShim hover:bg-orangeShimLighter">Send Email</button>      
             </div>
           </div>
         </form>
+        {/* Form End */}
       </div>
     </section>
   )
