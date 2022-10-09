ec116af5d1bf3d89b361cd562382a03623d74701
Shimiiy <bangchaq2002@gmail.com>
2022-10-09 13:14:37 +0900

    Made 404 page more abnormal (dark background)

--- pages/404.tsx
+++ pages/404.tsx
@@ -11,15 +11,15 @@ export default function Custom404() {
   return (
     <div>
       <Navbar />
-      <main className="container flex items-center px-6 mx-auto">
+      <main className="container flex items-center px-6 mx-auto bg-slate-700 rounded-lg py-16">
         <Head>
           <title>404 - Page not found!</title>
           <meta charSet='UTF-8'/>
           <meta name="description" content="Shimiiy's portfolio." />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <div className="flex mx-auto h-full flex-col md:flex-row p-6 w-full items-center">
-          <div className="p-6">
+        <div className="flex mx-auto h-full flex-col md:flex-row p-6 w-full items-center text-white">
+          <div className="p-6 mx-16">
             <Image src={PlantingImage} alt="Planting Image" width={270} height={300} />
           </div>
           <div className="text-center md:text-left">
@@ -33,7 +33,7 @@ export default function Custom404() {
                 <li>Artworks</li>
                 <li>Blog</li>
               </ul>
-              <p><span className="font-medium text-red-500">They&apos;re not ready just yet!</span> Sorry for the inconvinience!</p>
+              <p><span className="font-medium text-red-500">They&apos;re still in development and not ready yet!</span> Sorry for the inconvinience!</p>
               <p className="pt-6 font-semibold">In the meantime, please check out this site&apos;s <a 
                 href="https://github.com/Shimiiy/shimiiy-dev-site" 
                 className="underline text-sky-300 font-semibold hover:text-sky-500"
